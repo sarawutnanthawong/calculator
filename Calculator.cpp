9c718e96bbbfb527e9a59b429f40d9818b2bf328
Naphon intusai <npitz.ze@gmail.com>
2026-02-04 00:31:17 +0700

    main and multiply

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,1 +1,37 @@
 #include <stdio.h>
+
+int mul(int a, int b); 
+
+int main() {
+    int num1, num2, result;
+    char op;
+
+    printf("Simple Multiplication Calculator (*)\n");
+    
+    printf("Enter first num: ");
+    scanf("%d", &num1);
+
+    printf("Enter operator (*): ");
+    scanf(" %c", &op);   
+
+    printf("Enter second num: ");
+    scanf("%d", &num2);
+
+    if (op == '*') {
+        result = mul(num1, num2);
+    } else {
+        printf("Invalid operator! Please use '*'\n");
+        return 0;
+    }
+
+    printf("\nProcess:\n");
+    printf("%d %c %d\n", num1, op, num2);
+    printf("Result:\n");
+    printf("%d\n", result);
+
+    return 0;
+}
+
+int mul(int a, int b) {
+    return a * b;
+}
